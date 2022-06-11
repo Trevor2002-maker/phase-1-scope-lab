f09adef2f329c3b58336a8305992b303efc98d44
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2022-06-11 13:42:06 +0300

    commit tree 6a80646203995a494c617de80dc4162a0d9d9651

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your solution in this file!
+var customerName= "bob"
+
+function upperCaseCustomerName(name) {
+    var name = customerName.toUpperCase();
+    return name;
+}
+upperCaseCustomerName()
