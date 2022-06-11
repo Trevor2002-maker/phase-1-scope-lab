244f78a293769b1b1963ed7f4d95ac2dc2f21f0e
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2022-06-11 14:11:00 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+// global variable
+var customerName= "bob"
+
+// function to access global variable
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+ // best customer
+function setBestCustomer(){
+    bestCustomer='not bob';
+}
+
+//overwrite best customer
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+//least favorite customer
+const leastFavoriteCustomer = "Wendy"
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "Mandy"
+}
